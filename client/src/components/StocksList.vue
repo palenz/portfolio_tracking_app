79eb3a1f6bd452e3eb4ab91b366b7a34b0fdc1a1
Stanley Houcke <stanhoucke@gmail.com>
2021-01-24 12:57:12 +0000

    sharesSummary loops through portfolio

--- client/src/components/StocksList.vue
+++ client/src/components/StocksList.vue
@@ -24,7 +24,33 @@ export default {
       return this.portfolio.filter(function (item, index) {
         return !item.name 
       } )
-    }
+    },
+
+    sharesSummary: function() {
+      const sharesSummary = [];
+
+      for (let share of this.sharesList) {
+        sharesSummary.push({
+          symbol: share.symbol,
+          shares: share.shares
+        })
+        
+      }
+      // newArray = [
+        // {
+        //   symbol: KO,
+        //   shares: 5
+        // }
+      // ]
+
+      // loop through this.sharesList
+        // create an object with 2 keys symbol and shares
+      // add symbol and shares to new summary array
+      // if symbol exists, add shares
+      // if symbol does not exist, add symbol and shares
+
+      return sharesSummary
+  }
   }
 };
 </script>
