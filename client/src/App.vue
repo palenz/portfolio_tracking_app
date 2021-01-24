7a28a75507668f4bb7a2dcb0214f7e9a0f20cb6b
Juan <jpalenzuela@outlook.com>
2021-01-24 12:10:14 +0000

    added check latest price using user input

--- client/src/App.vue
+++ client/src/App.vue
@@ -11,6 +11,7 @@
 
       <stocks-list></stocks-list>  
       <chart-item></chart-item>
+      <stock-item :latestStockPrice='latestStockPrice'></stock-item>
 
     </main>
 
@@ -22,13 +23,50 @@
 <script>
 import StocksList from "./components/StocksList.vue";
 import ChartItem from "./components/ChartItem.vue";
+import StockItem from "./components/StockItem.vue";
+import { eventBus } from './main.js';
+
 export default {
   name: "app",
   components: {
     "stocks-list": StocksList,
     "chart-item": ChartItem,
+    "stock-item": StockItem
   },
+  data(){
+    return{
+      latestStockPrice: null,
+      selectedStock: ""
+    }
+  },
+  methods: {
+  
+    fetchStocks: function(ticker){
+      const url = `https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=${ticker}&outputsize=compact&apikey=C6DT0GSGVYAGQ4TA`
+    
+      fetch(url)
+      .then(res => res.json())
+      .then(data => {
+        const obj = data['Time Series (Daily)'];
+        const arr = Object.values(obj);
+        const latestEntry = arr[0];
+      this.latestStockPrice = latestEntry['4. close']    
+    })
+  }
+  },
+
+  mounted() {
+    eventBus.$on('stock-selected', (stock) => {
+      this.selectedStock = stock
+      this.fetchStocks(this.selectedStock)
+      })
+      
+      
+
+  }
 };
+
+
 </script>
 
 <style lang="css" scoped>
--- client/src/components/StockItem.vue
+++ client/src/components/StockItem.vue
@@ -1,9 +1,41 @@
 <template lang="html">
+<div>
+  <!-- {{latestStockPrice}} hi -->
   
+  <form v-on:submit.prevent='handleStockSubmit'>
+    <label for='selected-stock'>Enter New Ticker:</label>
+    <input type='text' id="selected-stock" v-model='selectedStock'/>
+    <input type='submit' value='Enter Ticker'/>
+  </form>
+
+  <p v-if='latestStockPrice'>The latest price for {{selectedStock}} is {{latestStockPrice}}</p>
+
+
+</div>
 </template>
 
 <script>
-export default {};
+import { eventBus } from '../main.js';
+
+export default {
+    
+    name: 'stock-item',
+    
+    props: ['latestStockPrice'],
+    
+    data () {
+        return {
+        selectedStock: ''
+        }
+    },
+
+    methods: {
+        handleStockSubmit(){
+            eventBus.$emit('stock-selected', this.selectedStock)
+        }
+    }
+
+};
 </script>
 
 <style lang="css" scoped>
