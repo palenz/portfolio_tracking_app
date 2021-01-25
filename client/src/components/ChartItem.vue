56fffc49900e673b85ce7c0dc2d729c6a1f236bc
jonnystv <jonathan@darefoundation.com>
2021-01-25 15:50:36 +0000

    Chart run with sample data

--- client/src/components/ChartItem.vue
+++ client/src/components/ChartItem.vue
@@ -1,13 +1,51 @@
 <template lang="html">
   <div>
-<div>Chart Item Goes Here</div>
+<div>
+  
+  <h2>Share Performance Chart</h2>
+
+  <GChart
+    type="LineChart"
+    :data="chartData"
+    :options="chartOptions"
+  />
+    
+
+</div>
   </div>
 </template> 
 
 <script>
+
+import { GChart } from 'vue-google-charts';
+
 export default {
   name: "chart-item",
-};
+
+  components: {
+    GChart
+  },
+
+  data() {
+    return {
+      chartData: [
+  [ 'date', 'KO' ],
+  [ '2021-01-10', 20 ],
+  [ '2021-01-11', 22 ],
+  [ '2021-01-12', 24 ],
+  [ '2021-01-13', 26 ],
+  [ '2021-01-14', 22 ]
+],
+      chartOptions: {
+        chart: {
+          Title: 'Share Performance',
+          subtitle: 'Last Quarter Daily Closing Price'
+        }
+      }  
+  }
+},
+}
+
 </script>
 
 <style lang="css" scoped>
